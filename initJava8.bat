c32fd8766b6cc7c3525517de7823ee6f0835d947
ianrae <ianrae88@gmail.com>
2017-04-08 12:39:43 -0400

    remove

--- initJava8.bat
+++
@@ -1,9 +0,0 @@
-
-set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101
-
-echo setting Maven..
-path = %path%;C:\iantools\maven\apache-maven-3.3.9\bin;C:\Program Files (x86)\GnuPG\bin
-
-rem pause
-rem create new app. run this in parent dir. it creates the project subdir
-rem mvn archetype:generate -DgroupId=com.github.ianrae -DartifactId=dnalparse -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
