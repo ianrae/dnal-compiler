550199633708049a42a6aa4a0375ea0f0e2fc01c
ianrae <ianrae88@gmail.com>
2017-04-08 13:17:43 -0400

    sample

---
+++ example/ss.bat
@@ -0,0 +1,5 @@
+rem -build the maven project
+rem -copy target/dnalc-0.2.0-jar-with-dependencies.jar to this directory
+rem -then the following command will run dnalc 
+ 
+java -jar dnalc-0.2.0-jar-with-dependencies.jar g -t=text/custom struct1.dnal
