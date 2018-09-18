388f83e4ade841df3b115edc073db2737fb9b932
ianrae <ianrae88@gmail.com>
2018-09-18 15:40:51 -0400

    dnalc copied from dnal-lang

---
+++ src/main/java/org/dnal/dnalc/other/dnalc/cmdline/Command.java
@@ -0,0 +1,9 @@
+package org.dnal.dnalc.cmdline;
+
+public class Command {
+	public String name;
+	public String srcPath;
+	public String importPath; //--dnalpath=dir1
+	public boolean debug;
+    public boolean perfSummaryEnabled;
+}
